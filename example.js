523a37a34806396ae66ca1a2288e241d72e21e44
Michael <michael.lafon@live.fr>
2016-10-21 13:37:57 +0800

    [feature/no-agent] rename test.js to example.js & add some comments.

--- example.js
+++ example.js
@@ -1,10 +1,13 @@
 const qsdk = require('./index')
 
+// Create a new client.
 const client = new qsdk.QoncreteClient({
     sourceID: 'SOURCE_ID',
     apiToken: 'API_TOKEN'
 })
 
+// Send a log with promises and extra options.
+// In this case, if the request timeout after 3s, it will retry one more time.
 client.send({ user: 'name', action: 'purchase', price: 99.99 }, { timeoutAfter: 3000, retryOnTimeout: 1 }).
     then(() => {
         console.log('SUCCESS')
@@ -13,5 +16,8 @@ client.send({ user: 'name', action: 'purchase', price: 99.99 }, { timeoutAfter: 
         console.log('ERROR:', ex)
     })
 
+// Fire & Forget
 client.send({ user: 'name', action: 'purchase', price: 99.99 })
+
+// Default options & callback style.
 client.send({ user: 'name', action: 'purchase', price: 99.99 }, {}, error => console.log(error || 'SUCCESS'))
